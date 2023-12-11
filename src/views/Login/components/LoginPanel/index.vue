780c890d85c7152ffec55215743ca61751e0b893
zhaojisen <10357640+zhaojisen@user.noreply.gitee.com>
2023-12-11 17:06:51 +0800

    Login 页面的基本搭建

--- src/views/Login/components/LoginPanel/index.vue
+++ src/views/Login/components/LoginPanel/index.vue
@@ -3,8 +3,8 @@
         <h1 class="title">后台管理系统</h1>
         <el-tabs
             stretch
-            class="login-tabs"
             type="border-card"
+            class="login-tabs"
             v-model="activeName"
             @tab-click="handleClick"
         >
@@ -13,13 +13,36 @@
                     <el-icon><User /></el-icon>
                     <span style="padding-left: 10px">账号登录</span>
                 </template>
-                账号登录
+                <el-form
+                    ref="ruleFormRef"
+                    class="account-form"
+                    status-icon
+                    :rules="rules"
+                    :model="ruleForm"
+                >
+                    <el-form-item label="账号" prop="account">
+                        <el-input
+                            clearable
+                            autocomplete="off"
+                            v-model="ruleForm.account"
+                        />
+                    </el-form-item>
+                    <el-form-item label="密码" prop="password">
+                        <el-input
+                            clearable
+                            type="password"
+                            autocomplete="off"
+                            v-model="ruleForm.password"
+                        />
+                    </el-form-item>
+                </el-form>
             </el-tab-pane>
-            <el-tab-pane nam="phone">
+            <el-tab-pane name="phone">
                 <template #label>
                     <el-icon><Iphone /></el-icon>
                     <span style="padding-left: 10px">手机登录</span>
                 </template>
+                手机登录
             </el-tab-pane>
         </el-tabs>
         <div class="control flex-align-center">
@@ -35,15 +58,32 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 import { TabsPaneContext } from 'element-plus';
 import { Iphone, User } from '@element-plus/icons-vue';
+import type { FormInstance, FormRules } from 'element-plus';
+
+const ruleFormRef = ref<FormInstance>();
 
 const activeName = ref('account');
 const isPersistent = ref(false);
+const ruleForm = reactive({
+    account: '',
+    password: ''
+});
+const rules = reactive<FormRules<typeof ruleForm>>({
+    account: [{ required: true, message: '请输入账号', trigger: 'blur' }],
+    password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
+});
 
-const handleClick = (tab: TabsPaneContext, event: Event) => {
-    console.log(tab, event);
+const handleClick = (tab: TabsPaneContext) => {
+    if (tab.paneName === 'account') {
+        console.log('account 登录');
+    }
+
+    if (tab.paneName === 'phone') {
+        console.log('phone 登录');
+    }
 };
 
 const handleLogin = () => {};
